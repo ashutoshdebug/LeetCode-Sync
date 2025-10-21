76859cf9b884bd52f05cc2249e965ee94d926b1b
Ashutosh Kumar Tiwari <126601693+ashutoshdebug@users.noreply.github.com>
2025-10-21 21:37:07 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (98.14%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -3,6 +3,7 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
         int start = 0;
+        // Sync
 
         for(int i = 0; i<n; i++){
             if (nums[i] != 0)
